2aea87a1d6ce44f7d744bdf305985ef1fdfc1b05
Michael <mikeks81@gmail.com>
2016-07-18 20:08:35 -0400

    fixed job form upload bug

--- app/assets/javascripts/jobs.js
+++ app/assets/javascripts/jobs.js
@@ -1,23 +1,40 @@
-var paymentButton = document.getElementById('payment_button');
-	paymentButton.addEventListener('submit', jobAjax());
+// var paymentButton = document.getElementById('payment_button');
+// 	paymentButton.addEventListener('submit', jobAjax());
 
-var expenseButton = document.getElementById();
-	expenseButton.addEventListener('submit', jobAjax());
+// var expenseButton = document.getElementById();
+// 	expenseButton.addEventListener('submit', jobAjax());
 
-function jobAjax(){
-	$.ajax({
-		type: "GET",
-		contentType: "application/json; charset=utf-8",
-		url: '',
-		dataType: 'json',
-		success: function(data){
-			console.log(data)
-		},
-		error: function() {
 
-		}
-	});
-}
+// $(function(){
+// 	$("#payment_form").bind("ajax:complete", 
+// 		function(event,xhr,status){
+// 			console.log('gibbbdidddy');
+//   			$('#payment_type').val('');
+//   			$('#payment_amount').val('');
+//   			$('#payment_date').val('');
+// 		})
+
+// 	var jobId = document.getElementById('job_id');
+// 	jobId = jobId.getAttribute("data-job-id");
+// });
+
+
+// function jobAjax(){
+// 	console.log('here');
+// 	$.ajax({
+// 		type: "GET",
+// 		contentType: "application/json; charset=utf-8",
+// 		url: '/clients/:client_id/jobs/' + jobId,
+// 		dataType: 'json',
+// 		success: function(data){
+// 			console.log(data)
+// 		},
+// 		error: function() {
+
+// 		}
+// 	});
+// }
+
 
 
 
