674fcb017694c5fafa3af3c9100bd4ccc6dbf35a
Michael <mikeks81@gmail.com>
2016-07-18 22:42:00 -0400

    worked on some big decimal ajax converting without success and app now sends email to client when signing up and send me one as well.

--- app/mailers/job_pdf.rb
+++ app/mailers/job_pdf.rb
@@ -16,9 +16,15 @@ class JobPdf < ApplicationMailer
  #  	end
 
   def send_signup_email(user)
+  	byebug
 	@user = user
 	mail( :to => @user.email,
-	:subject => 'Thanks for signing up for our amazing app' )
+	:subject => 'Thanks for signing up for Freelance Me.' )
+
+	# send email to me everytime someone signs up
+	mail( :to => 'mikeks81@gmail.com',
+	 :subject => "Someone Just signed up at Freelanceme.",
+	 :body => @user.fname + " " + @user.lname  + " " + @user.email)
   end
 
 end
